4e6ea4f2df96640140a1d9be5be0f9e09eb07b7d
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-03-04 22:13:40 +0200

    Update users.js

--- pages/en/users.js
+++ pages/en/users.js
@@ -35,7 +35,7 @@ class Users extends React.Component {
             </div>
             <div className="logos">{showcase}</div>
             <p>Just as web accessibility and SEO are linked, so too are web accessibility and usability.If you’re ready to launch a full accessibility initiative or have questions, you can reach out to our team of experts for a free 30-minute consultation about your next steps.</p>
-            <a href=https:/uahub.org.za/ className="button">
+            <a href={https:/uahub.org.za/} className="button">
               Add your company
             </a>
           </div>
