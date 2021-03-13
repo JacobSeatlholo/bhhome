9494559c2de8b0b1b148077dcb88f3311f808479
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-03-13 09:26:04 +0200

    Update Footer.js

--- core/Footer.js
+++ core/Footer.js
@@ -37,7 +37,7 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>General</h5>
-            <a href={this.docUrl('terms.', this.props.language)}>
+            <a href={this.docUrl('terms', this.props.language)}>
               Terms and conditions
             </a>
             <a href={this.docUrl('doc2.html', this.props.language)}>
