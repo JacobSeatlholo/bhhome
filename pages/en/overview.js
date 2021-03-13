3bcea6bbdaadb982db8496f97db77d48b291fa01
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-03-13 12:04:43 +0200

    Create overview.js

---
+++ pages/en/overview.js
@@ -0,0 +1,24 @@
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary.js');
+
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
+
+function HelloWorld(props) {
+  return (
+    <div className="docMainWrapper wrapper">
+      <Container className="mainContainer documentContainer postContainer">
+        <h1>Hello World!</h1>
+        <p>This is my first page!</p>
+    
+    <iframe width="420" height="315"
+src="https://www.youtube.com/embed/tgbNymZ7vqY">
+</iframe>
+    
+      </Container>
+    </div>
+  );
+}
+
+module.exports = HelloWorld;
