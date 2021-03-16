1d165aec2c093d261a46476f02f5c2c64bd57677
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-03-16 18:37:00 +0200

    Update EULA.js

--- pages/en/EULA.js
+++ pages/en/EULA.js
@@ -9,8 +9,33 @@ function HelloWorld(props) {
   return (
     <div className="docMainWrapper wrapper">
       <Container className="mainContainer documentContainer postContainer">
-        <h1>Hello World!</h1>
-        <p>This is my first page!</p>
+        <h1>End-User License Agreement</h1>
+        <p>End-User License Agreement (EULA) of Business-Hustle
+  
+This End-User License Agreement ("EULA") is a legal agreement between you and Simple eternity holdings (Pty) Ltd 
+This EULA agreement governs your acquisition and use of our business-hustle.co software ("Software") directly from Simple eternity holdings (Pty) Ltd or indirectly through a Simple eternity holdings (Pty) Ltd authorized reseller or distributor (a "Reseller").
+Please read this EULA agreement carefully before completing the installation process and using the business-hustle.co software. It provides a license to use the business-hustle.co software and contains warranty information and liability disclaimers.
+If you register for a free trial of the business-hustle.co software, this EULA agreement will also govern that trial. By clicking "accept" or installing and/or using the business-hustle.co software, you are confirming your acceptance of the Software and agreeing to become bound by the terms of this EULA agreement.
+If you are entering into this EULA agreement on behalf of a company or other legal entity, you represent that you have the authority to bind such entity and its affiliates to these terms and conditions. If you do not have such authority or if you do not agree with the terms and conditions of this EULA agreement, do not install or use the Software, and you must not accept this EULA agreement.
+This EULA agreement shall apply only to the Software supplied by Simple eternity holdings (Pty) Ltd herewith regardless of whether other software is referred to or described herein. The terms also apply to any Simple eternity holdings (Pty) Ltd updates, supplements, Internet-based services, and support services for the Software, unless other terms accompany those items on delivery. If so, those terms apply. 
+<br><br>
+  <h2>License Grant<h2>
+Simple eternity holdings (Pty) Ltd hereby grants you a personal, non-transferable, non-exclusive licence to use the business-hustle.co software on your devices in accordance with the terms of this EULA agreement.
+You are permitted to load the business-hustle.co software (for example a PC, laptop, mobile or tablet) under your control. You are responsible for ensuring your device meets the minimum requirements of the business-hustle.cosoftware.
+You are not permitted to:
+Edit, alter, modify, adapt, translate or otherwise change the whole or any part of the Software nor permit the whole or any part of the Software to be combined with or become incorporated in any other software, nor decompile, disassemble or reverse engineer the Software or attempt to do any such things
+Reproduce, copy, distribute, resell or otherwise use the Software for any commercial purpose
+Allow any third party to use the Software on behalf of or for the benefit of any third party
+Use the Software in any way which breaches any applicable local, national or international law
+use the Software for any purpose that Simple eternity holdings (Pty) Ltd considers is a breach of this EULA agreement
+Intellectual Property and Ownership
+Simple eternity holdings (Pty) Ltd shall at all times retain ownership of the Software as originally downloaded by you and all subsequent downloads of the Software by you. The Software (and the copyright, and other intellectual property rights of whatever nature in the Software, including any modifications made thereto) are and shall remain the property of Simple eternity holdings (Pty) Ltd .
+Simple eternity holdings (Pty) Ltd reserves the right to grant licences to use the Software to third parties.
+Termination
+This EULA agreement is effective from the date you first use the Software and shall continue until terminated. You may terminate it at any time upon written notice to Simple eternity holdings (Pty) Ltd .
+It will also terminate immediately if you fail to comply with any term of this EULA agreement. Upon such termination, the licenses granted by this EULA agreement will immediately terminate and you agree to stop all access and use of the Software. The provisions that by their nature continue and survive will survive any termination of this EULA agreement.
+Governing Law
+This EULA agreement, and any dispute arising out of or in connection with this EULA agreement, shall be governed by and construed in accordance with the laws of za.</p>
       </Container>
     </div>
   );
