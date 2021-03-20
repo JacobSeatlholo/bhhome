1204825dc0acd586f9f9a3c97a0677cc5e758367
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-03-20 11:10:28 +0200

    Update sms.js

--- pages/en/sms.js
+++ pages/en/sms.js
@@ -11,6 +11,7 @@ function HelloWorld(props) {
       <Container className="mainContainer documentContainer postContainer">
         <h1>Hello World!</h1>
         <p>This is my first page!</p>
+<p id="demo"></p>
     <script>document.getElementById("demo").innerHTML = "My First JavaScript";
 </script>
       </Container>
