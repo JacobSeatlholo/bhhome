125620849c606fdb5f29b7e4a50afa6cf2fd3eb4
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-03-20 11:47:08 +0200

    Update sms.js

--- pages/en/sms.js
+++ pages/en/sms.js
@@ -14,6 +14,11 @@ function HelloWorld(props) {
 <p id="demo"></p>
     <script>document.getElementById("demo").innerHTML = "My First JavaScript";
 </script>
+  
+  <iframe height="265" style="width: 100%;" scrolling="no" title="Kendo UI Calendar - Scheduler" src="https://codepen.io/tyler-g/embed/rZMNQj?height=265&theme-id=light&default-tab=js,result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+  See the Pen <a href='https://codepen.io/tyler-g/pen/rZMNQj'>Kendo UI Calendar - Scheduler</a> by Tyler G. Yazzie
+  (<a href='https://codepen.io/tyler-g'>@tyler-g</a>) on <a href='https://codepen.io'>CodePen</a>.
+</iframe>
       </Container>
     </div>
   );
