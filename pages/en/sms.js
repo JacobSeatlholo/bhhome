226a189f3763d941aed0220733d2296336e0d234
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-03-21 15:37:57 +0200

    Update sms.js

--- pages/en/sms.js
+++ pages/en/sms.js
@@ -11,13 +11,129 @@ function HelloWorld(props) {
       <Container className="mainContainer documentContainer postContainer">
         <h1>Hello World!</h1>
    
-    <script>document.getElementById("demo")".innerHTML = "My First JavaScript";
-</script>
-  
-  <iframe height="265" style="width: 100%;" scrolling="no" title="Kendo UI Calendar - Scheduler" src="https://codepen.io/tyler-g/embed/rZMNQj?height=265&theme-id=light&default-tab=js,result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
-  See the Pen <a href='https://codepen.io/tyler-g/pen/rZMNQj'>Kendo UI Calendar - Scheduler</a> by Tyler G. Yazzie
-  (<a href='https://codepen.io/tyler-g'>@tyler-g</a>) on <a href='https://codepen.io'>CodePen</a>.
-</iframe>"
+$(function() {
+    $("#scheduler").kendoScheduler({
+        date: new Date("2013/6/13"),
+        startTime: new Date("2013/6/13 07:00 AM"),
+        height: 600,
+        views: [
+            "day",
+            { type: "workWeek", selected: true },
+            "week",
+            "month",
+            "agenda",
+            { type: "timeline", eventHeight: 50}
+        ],
+        timezone: "Etc/UTC",
+        dataSource: {
+            batch: true,
+            transport: {
+                read: {
+                    url: "//demos.telerik.com/kendo-ui/service/tasks",
+                    dataType: "jsonp"
+                },
+                update: {
+                    url: "//demos.telerik.com/kendo-ui/service/tasks/update",
+                    dataType: "jsonp"
+                },
+                create: {
+                    url: "//demos.telerik.com/kendo-ui/service/tasks/create",
+                    dataType: "jsonp"
+                },
+                destroy: {
+                    url: "//demos.telerik.com/kendo-ui/service/tasks/destroy",
+                    dataType: "jsonp"
+                },
+                parameterMap: function(options, operation) {
+                    if (operation !== "read" && options.models) {
+                        return {models: kendo.stringify(options.models)};
+                    }
+                }
+            },
+            schema: {
+                model: {
+                    id: "taskId",
+                    fields: {
+                        taskId: { 
+                          from: "TaskID", 
+                          type: "number" },
+                        title: { 
+                          from: "Title", 
+                          defaultValue: "No title", 
+                          validation: { 
+                            required: true } },
+                        start: { 
+                          type: "date", 
+                          from: "Start" },
+                        end: { 
+                          type: "date", 
+                          from: "End" },
+                        startTimezone: { 
+                          from: "StartTimezone" },
+                        endTimezone: { 
+                          from: "EndTimezone" },
+                        description: { 
+                          from: "Description" },
+                        recurrenceId: { 
+                          from: "RecurrenceID" },
+                        recurrenceRule: { 
+                          from: "RecurrenceRule" },
+                        recurrenceException: { 
+                          from: "RecurrenceException" },
+                        ownerId: { 
+                          from: "OwnerID", 
+                          defaultValue: 1 },
+                        isAllDay: { 
+                          type: "boolean", 
+                          from: "IsAllDay" }
+                    }
+                }
+            },
+            filter: {
+                logic: "or",
+                filters: [
+                    { field: "ownerId", 
+                     operator: "eq", 
+                     value: 1 },
+                    { field: "ownerId", 
+                     operator: "eq", 
+                     value: 2 }
+                ]
+            }
+        },
+        resources: [
+            {
+                field: "ownerId",
+                title: "Owner",
+                dataSource: [
+                    { text: "Alex", 
+                     value: 1, 
+                     color: "#f8a398" },
+                    { text: "Bob", 
+                     value: 2, 
+                     color: "#51a0ed" },
+                    { text: "Charlie", 
+                     value: 3,
+                     color: "#56ca85" }
+                ]
+            }
+        ]
+    });
+
+    $("#people :checkbox").change(function(e) {
+        var checked = $.map($("#people :checked"), function(checkbox) {
+            return parseInt($(checkbox).val());
+        });
+
+        var scheduler = $("#scheduler").data("kendoScheduler");
+
+        scheduler.dataSource.filter({
+            operator: function(task) {
+                return $.inArray(task.ownerId, checked) >= 0;
+            }
+        });
+    });
+});
       </Container>
     </div>
   );
